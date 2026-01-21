31956598d1478fd918576f846fee0907707b8bd6
lawr3ncey <babelonia.l.bscs@gmail.com>
2026-01-21 22:58:26 +0800

    Fix dependencies and React 19 compatibility for Vercel deployment

---
+++ src/react-icons.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-icons/fa' {
+  import { FC, SVGProps } from 'react';
+  
+  export const FaPhone: FC<SVGProps<SVGSVGElement>>;
+  export const FaEnvelope: FC<SVGProps<SVGSVGElement>>;
+  export const FaGlobe: FC<SVGProps<SVGSVGElement>>;
+  export const FaMapMarkerAlt: FC<SVGProps<SVGSVGElement>>;
+  export const FaLinkedinIn: FC<SVGProps<SVGSVGElement>>;
+  export const FaGithub: FC<SVGProps<SVGSVGElement>>;
+  export const FaFacebookF: FC<SVGProps<SVGSVGElement>>;
+  export const FaInstagram: FC<SVGProps<SVGSVGElement>>;
+}
